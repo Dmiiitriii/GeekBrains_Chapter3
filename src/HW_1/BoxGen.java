3c89c30c5c095c80ccbad33e3ddfd4325fa6cde4
Dmitrii <dmitry.ongirskiy@gmail.com>
2020-10-21 18:51:50 +0300

    HW_3-1

---
+++ src/HW_1/BoxGen.java
@@ -0,0 +1,17 @@
+package HW_1;
+
+public class BoxGen<T> {
+    private T obj;
+
+    public BoxGen(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+}
